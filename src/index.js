f3702ed0dce4d7edf8bd9c22939c5e383e4584c5
AlekseiMinsk <budanov.info@gmail.com>
2019-02-22 20:29:57 +0300

    task complited100

--- src/index.js
+++ src/index.js
@@ -3,5 +3,34 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+var arr = [...preferences]
+
+ if(arr.length < 3) return 0;
+ var answer = 0;
+ var alreadyInLove = [];
+ arr.forEach((item, index) => {
+  var n = alreadyInLove.includes(index);
+  if(!n) {
+   var secondLoveIndex = item - 1;
+   if(secondLoveIndex != index) {
+    var secondLoveItem = arr[secondLoveIndex];
+    if(secondLoveItem) {
+     var thirdLoveIndex = secondLoveItem - 1;
+     if(thirdLoveIndex != secondLoveIndex && thirdLoveIndex != index) {
+      var thirdLoveItem = arr[thirdLoveIndex];
+      if(thirdLoveItem) {
+       if((thirdLoveItem - 1) === index) {
+        answer++;
+                             alreadyInLove = [...alreadyInLove, index, secondLoveIndex, thirdLoveIndex];
+
+       }
+      }
+     }
+
+    }
+   }
+  } 
+ });
+ return answer;
 };
